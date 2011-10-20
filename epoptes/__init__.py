308c06a99716056ca78d45f910edc4a08692c187
Alkis Georgopoulos <alkisg@gmail.com>
2011-10-20 10:01:53 +0300

    Version 0.3.0

--- epoptes/__init__.py
+++ epoptes/__init__.py
@@ -9,6 +9,6 @@ PARTICULAR PURPOSE. See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with 
 Epoptes. If not, see <http://www.gnu.org/licenses/>.'''
 
-__version__ = "0.3b3"
+__version__ = "0.3.0"
 __license__ = "GPL"
 
