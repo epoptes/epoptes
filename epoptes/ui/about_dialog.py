c3734ad59d3b8290b2e0c86be193a01bac3c67ff
Alkis Georgopoulos <alkisg@gmail.com>
2018-05-12 10:30:19 +0300

    gtk2to3: about_dialog.py, about_dialog.ui.

--- epoptes/ui/about_dialog.py
+++ epoptes/ui/about_dialog.py
@@ -5,6 +5,7 @@
 # About dialog.
 #
 # Copyright (C) 2010 Fotis Tsamis <ftsamis@gmail.com>
+# 2018, Alkis Georgopoulos <alkisg@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -23,26 +24,22 @@
 # Public License can be found in `/usr/share/common-licenses/GPL".
 ###########################################################################
 
-import gtk
-import pygtk
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 
 from epoptes import __version__
 
+
 class About:
     def __init__(self):
-        self.wTree = gtk.Builder()
+        self.wTree = Gtk.Builder()
         self.wTree.add_from_file('about_dialog.ui')
         self.wTree.connect_signals(self)
-        self.get = self.wTree.get_object
-        
-        self.dialog = self.get('aboutdialog')
-        logo = gtk.gdk.pixbuf_new_from_file_at_size(
-            '../icons/hicolor/scalable/apps/epoptes.svg', 64, 64)
-        self.dialog.set_logo(logo)
+
+        self.dialog = self.wTree.get_object('aboutdialog')
         self.dialog.set_version(__version__)
-        self.dialog.set_translator_credits(_("translator-credits"))
-        self.dialog.set_artists(["Andrew Wedderburn (application icon)"])
-    
+
     def run(self):
         self.dialog.run()
         self.dialog.destroy()
