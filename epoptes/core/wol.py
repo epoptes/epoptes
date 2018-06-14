788c06e718bbd84a87529517cc3be45ab5e1cd65
Alkis Georgopoulos <alkisg@gmail.com>
2018-06-14 10:22:21 +0300

    restructure: wol.py.

--- epoptes/core/wol.py
+++ epoptes/core/wol.py
@@ -1,35 +1,20 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-###########################################################################
-# Wake on LAN.
-#
-# Copyright (C) 2010-2018 Alkis Georgopoulos <alkisg@gmail.com>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# On Debian GNU/Linux systems, the complete text of the GNU General
-# Public License can be found in `/usr/share/common-licenses/GPL".
-###########################################################################
-
-import sys
+# This file is part of Epoptes, http://epoptes.org
+# Copyright 2018 the Epoptes team, see AUTHORS.
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""
+Power on remote computers using Wake On LAN.
+"""
 import socket
-import struct
+import sys
+
 import netifaces
 
 
-def getBroadcastList():
+def get_broadcast_list():
+    """Return all broadcast addresses.
+    E.g. WOL messages need to be sent from all NICs.
+    """
     brlist = ['<broadcast>']
     ifaces = [iface for iface in netifaces.interfaces() if iface != 'lo']
     for ifname in ifaces:
@@ -45,9 +30,8 @@ def getBroadcastList():
 
 
 def wake_on_lan(macaddress):
-    """ Switches on remote computers using WOL. """
-
-    # Check macaddress format and try to compensate.
+    """Power on remote computers using Wake On LAN."""
+    # Handle MACs with or without separators.
     if len(macaddress) == 12:
         pass
     elif len(macaddress) == 12 + 5:
@@ -57,22 +41,20 @@ def wake_on_lan(macaddress):
         raise ValueError('Incorrect MAC address format')
 
     print("Sending magic packet to", macaddress)
-    # Pad the synchronization stream.
-    data = ''.join(['FFFFFFFFFFFF', macaddress * 20])
-    send_data = ''
-
-    # Split up the hex values and pack.
-    for i in range(0, len(data), 2):
-        send_data = ''.join([send_data,
-                             struct.pack('B', int(data[i: i + 2], 16))])
+    packet = bytes.fromhex(''.join(['FFFFFFFFFFFF', macaddress * 20]))
 
     # Broadcast it to the LAN.
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    for br in getBroadcastList():
-        sock.sendto(send_data, (br, 9))
+    for brd in get_broadcast_list():
+        sock.sendto(packet, (brd, 9))
+
+
+def main():
+    """Run the module from the command line."""
+    for mac in sys.argv[1:]:
+        wake_on_lan(mac)
 
 
 if __name__ == '__main__':
-    for mac in sys.argv[1:]:
-        wake_on_lan(mac)
+    main()
