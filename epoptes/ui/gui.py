9a641a5d83551a0f053f6750b9434cd12d9059ad
Alkis Georgopoulos <alkisg@gmail.com>
2018-07-24 12:51:54 +0300

    restructure gui: sanitize imports.

--- epoptes/ui/gui.py
+++ epoptes/ui/gui.py
@@ -1,66 +1,32 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-###########################################################################
-# GUI.
-#
-# Copyright (C) 2010 Fotis Tsamis <ftsamis@gmail.com>
-# 2018, Alkis Georgopoulos <alkisg@gmail.com>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# On Debian GNU/Linux systems, the complete text of the GNU General
-# Public License can be found in `/usr/share/common-licenses/GPL".
-###########################################################################
-
-
-from epoptes.ui.common import gettext as _
-from gi.repository import Gtk
-from gi.repository import Gdk
-from gi.repository import GLib
-from gi.repository import GdkPixbuf
+# This file is part of Epoptes, http://epoptes.org
+# Copyright 2010-2018 the Epoptes team, see AUTHORS.
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""
+Epoptes GUI class.
+"""
 from distutils.version import LooseVersion
-import os
-import dbus
-import logging
-import sys
-import shlex
-import pipes
 import getpass
-import socket
 import locale
-from twisted.internet import gtk3reactor
-# This must be run after epoptes.common.config (=Gtk imported without version)
-# and before twisted.internet.reactor (=reactor already installed)
-gtk3reactor.install()
-from twisted.internet import reactor
-from twisted.python import log
+import os
+import pipes
+import socket
 
-from .. import ui
-from .. import __version__
-from ..common.constants import *
-from ..common import config
-from ..core import structs
-from ..core import wol
-from ..core.lib_users import *
-from ..daemon import uiconnection
-from .about import About
-from .benchmark import Benchmark
-from .client_information import ClientInformation
-from .exec_command import ExecCommand
-from .notifications import NotifyQueue
-from .send_message import SendMessage
+from epoptes.ui.reactor import reactor
+from epoptes.common.constants import *
+from epoptes.common import config
+from epoptes.core import structs
+from epoptes.core import wol
+from epoptes.core.lib_users import *
+from epoptes.ui.about import About
+from epoptes.ui.benchmark import Benchmark
+from epoptes.ui.client_information import ClientInformation
+from epoptes.ui.common import gettext as _
+from epoptes.ui.exec_command import ExecCommand
+from epoptes.ui.notifications import NotifyQueue
+from epoptes.ui.send_message import SendMessage
+
+from gi.repository import Gdk, GdkPixbuf, GLib, Gtk
 
 
 class EpoptesGui(object):
---
+++ epoptes/ui/reactor.py
@@ -0,0 +1,12 @@
+# This file is part of Epoptes, http://epoptes.org
+# Copyright 2010-2018 the Epoptes team, see AUTHORS.
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""
+A small workaround to avoid many pylint3 import-related warnings.
+"""
+import epoptes.ui.common
+from twisted.internet import gtk3reactor
+# This must be run after epoptes.ui.common (=Gtk imported without version)
+# and before twisted.internet.reactor (=reactor already installed)
+gtk3reactor.install()
+from twisted.internet import reactor
